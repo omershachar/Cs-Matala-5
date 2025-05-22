efaec50c810e47536d469db1e2966e1d502db5b9
omer <omershachar98@icloud.com>
2025-05-22 17:38:27 +0300

    added IsEmailValid(string email) method to Validator.cs

--- Validator.cs
+++ Validator.cs
@@ -1,4 +1,10 @@
-namespace Assignment5.Utils
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Utilities
 {
     public static class Validator
     {
@@ -15,6 +21,13 @@ namespace Assignment5.Utils
         {
             if (string.IsNullOrWhiteSpace(code) || code.Length != 7 || code[0] != '#') return false;
             return code.Skip(1).All(c => char.IsDigit(c) || (c >= 'A' && c <= 'F'));
+        }
+
+        public static bool IsEmailValid(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+            var parts = email.Split('@');
+            return parts.Length == 2 && parts[0].Length > 0 && parts[1].Contains('.');
         }
     }
 }
